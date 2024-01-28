3648a99af9396838b94d4719ae2a829310e2f7d5
rnjsals0905@gmail.com <rnjsals0905@ajou.ac.kr>
2024-01-28 13:27:50 +0900

    [EDIT] Spring Security 관련 로그인 설정, User Entity 수정

--- OnlineShopping/src/main/java/com/forStudy/OnlineShopping/data/entity/User.java
+++ OnlineShopping/src/main/java/com/forStudy/OnlineShopping/data/entity/User.java
@@ -5,9 +5,19 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.util.Date;
+
 @Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userId;
+    private Long USER_KEY;
+    private String USER_ID;
+    private String PASSWORD;
+    private String NAME;
+    private String EMAIL;
+    private String PHONE_NUMBER;
+    private String ADDRESS;
+    private Date REG_DATE;
+    private String GRADE;
 }
